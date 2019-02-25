bf13589c0761eed9734b4ba5a7cad79536398f5e
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-25 12:34:36 +0000

    Added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+export default class MuseumNav extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="black" light expand="md">
+                    <NavbarBrand href="/" className="mr-auto">Museu Casal Aleixo</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href="google.com" className="mr-auto">Google</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
