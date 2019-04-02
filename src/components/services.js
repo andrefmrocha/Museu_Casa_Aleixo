59b6c0b2e6ad3236053f2e998f27f472f31c093f
Andre Rocha <andrefmrocha@live.com.pt>
2019-04-02 15:49:22 +0100

    Added services button

---
+++ src/components/services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Collapse from './collapse';
+
+
+const Services = () => (
+    <Collapse title="Services">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+        richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes
+        anderson cred nesciunt sapiente ea proident.
+  </Collapse>
+);
+
+export default Services;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Story from "../components/story"
+import Services from "../components/services"
 import Presentation from "../components/presentation";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Presentation/>
     <Story/>
+    <Services/>
   </Layout>
 )
 
